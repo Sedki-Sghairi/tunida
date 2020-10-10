860a78cdb86ba39a8fc39aadfdb48f0108383f2a
Sedki Sghairi <sedkisghairi017@gmail.com>
2020-10-10 16:06:46 +0100

    redux state management

---
+++ backend/data.js
@@ -0,0 +1,124 @@
+export default {
+	products: [
+		{
+			id: '1',
+			category: 'shirt',
+			title: 'tshirt slim',
+			price: 23,
+			brand: 'nike',
+			rating: 4,
+			reviews: 28,
+			image: '/images/d1.jpg',
+			stock: 0,
+			description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet.'
+		},
+		{
+			id: '2',
+			category: 'shirt',
+			title: 'Yellow hablih',
+			price: 15,
+			brand: 'nike',
+			rating: 3.5,
+			reviews: 28,
+			image: '/images/d2.jpg',
+			stock: 10,
+			description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet.'
+		},
+		{
+			id: '3',
+			category: 'shirt',
+			title: 'face no care',
+			price: 13,
+			brand: 'nike',
+			rating: 4.5,
+			reviews: 28,
+			image: '/images/d3.jpg',
+			stock: 5,
+			description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet.'
+		},
+		{
+			id: '4',
+			category: 'shirt',
+			title: 'black t-shirt',
+			price: 2.3,
+			brand: 'levis',
+			rating: 4,
+			reviews: 38,
+			image: '/images/d4.jpg',
+			stock: 6,
+			description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet.'
+		},
+		{
+			id: '5',
+			category: 'shirt',
+			title: 'jaddy kind',
+			price: 17,
+			brand: 'nike',
+			rating: 8,
+			reviews: 28,
+			image: '/images/d5.jpg',
+			stock: 7,
+			description: 'best deal'
+		},
+		{
+			id: '6',
+			category: 'shirt',
+			title: 'mother land',
+			price: 14,
+			brand: 'levis',
+			rating: 10,
+			reviews: 20,
+			image: '/images/d6.jpg',
+			stock: 4,
+			description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet.'
+		},
+		{
+			id: '7',
+			category: 'shirt',
+			title: 'black t-shirt',
+			price: 2,
+			brand: 'nike',
+			rating: 4,
+			reviews: 28,
+			image: '/images/d7.jpg',
+			stock: 3,
+			description: 'best deal'
+		},
+		{
+			id: '8',
+			category: 'shirt',
+			title: 'tshirt grey',
+			price: 3,
+			brand: 'levis',
+			rating: 5,
+			reviews: 18,
+			image: '/images/d8.jpg',
+			stock: 9,
+			description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet.'
+		},
+		{
+			id: '9',
+			category: 'shirt',
+			title: 'tshirt cartoon',
+			price: 7,
+			brand: 'levis',
+			rating: 8,
+			reviews: 10,
+			image: '/images/d9.jpg',
+			stock: 8,
+			description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet.'
+		},
+		{
+			id: '10',
+			category: 'shirt',
+			title: 'tshirt white',
+			price: 20,
+			brand: 'nike',
+			rating: 3,
+			reviews: 5,
+			image: '/images/d10.jpg',
+			stock: 10,
+			description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet.'
+		}
+	]
+};
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import data from './data';
+const app = express();
+
+app.get('/api/products', (req, res) => {
+	res.send(data.products);
+});
+app.get('/api/products/:id', (req, res) => {
+	const productId = req.params.id;
+	const product = data.products.find((x) => x.id === productId);
+	product ? res.send(product) : res.status(404).send({ msg: 'Product Not Found.' });
+});
+
+
+app.listen(5000, () => {
+	console.log('server started at: http://localhost:5000');
+});
