43ba2532d9cd18a16646f434b9c13938bc32400d
Sedki Sghairi <sedkisghairi017@gmail.com>
2020-10-29 20:59:09 +0100

    imported @babel/polyfill

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-
+import express from 'express'
+import '@babel/polyfill'
 import config from './config'
 import mongoose from 'mongoose'
 import userRoute from './routes/userRoute'
