87b095ca172a5f87b8c086953f24591883a5085c
Sedki Sghairi <sedkisghairi017@gmail.com>
2020-10-30 20:41:24 +0100

    ignoring .env

---
+++ dist/models/userModels.js
@@ -0,0 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const userSchema = new _mongoose.default.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+});
+
+const UserModel = _mongoose.default.model('User', userSchema);
+
+var _default = UserModel;
+exports.default = _default;
