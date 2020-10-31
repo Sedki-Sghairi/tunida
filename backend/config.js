e16e6dad8417d29de18e08c2d6dee857f9d01492
Sedki Sghairi <sedkisghairi017@gmail.com>
2020-10-31 14:59:08 +0100

    remove local .env

--- backend/config.js
+++ backend/config.js
@@ -4,4 +4,5 @@ export default {
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/ida',
     JWT_SECRET: process.env.JWT_SECRET ||  'quelquechosesecret',
     PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb'
+    
 }
