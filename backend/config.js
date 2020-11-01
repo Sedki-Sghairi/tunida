25f0c900d95e563a875f6683b711dabeee836b74
Sedki Sghairi <sedkisghairi017@gmail.com>
2020-11-01 12:49:54 +0100

    mongodb connect

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,15 @@
 import dotenv from 'dotenv'
 dotenv.config()
+// export default {
+  
+//     MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://sedki:sedkiDb@cluster0.y95bs.mongodb.net/sedki?retryWrites=true&w=majority',
+//     JWT_SECRET: process.env.JWT_SECRET ||  'quelquechosesecret',
+   
+    
+// }
 export default {
+    PORT : process.env.PORT || 5000,
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/ida',
     JWT_SECRET: process.env.JWT_SECRET ||  'quelquechosesecret',
     PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb'
-    
 }
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ import orderRoute from './routes/orderRoute'
 import "core-js";
 import "regenerator-runtime/runtime.js";
 import "@babel/polyfill";
-const path = require('path')
+import path from 'path'
+
 const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl, {
 	useNewUrlParser: true,
@@ -18,23 +19,22 @@ mongoose.connect(mongodbUrl, {
 }).catch(error => console.error(error.reason))
 
 const app = express();
+
 app.use(bodyParser.json())
 app.use('/api/users' , userRoute)
 app.use('/api/products' , productRoute)
 app.use('/api/orders', orderRoute);
 
 const CLIENT_ID = config.PAYPAL_CLIENT_ID
-
 app.get('/api/config/paypal', (req, res) =>{
 	res.send(CLIENT_ID)
 })
-   
-app.use(express.static(path.join(__dirname,'../frontend/build')))
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '../frontend/build/index.html'))
-  })
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+});
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log(`Mixing it up on port ${PORT}`)
-})
+app.listen(config.PORT, () => {
+	console.log('Server started at http://localhost:5000');
+  });
+  
